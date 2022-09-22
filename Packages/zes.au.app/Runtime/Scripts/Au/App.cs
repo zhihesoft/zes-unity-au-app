8f180874830bd557588614879ccc179a0fb868f6
stanley <stanley@zhihesoft.com>
2022-09-22 21:20:24 +0800

    add app

---
+++ Packages/zes.au.app/Runtime/Scripts/Au/App.cs
@@ -0,0 +1,26 @@
+﻿using Au.DI;
+using Au.TS;
+using UnityEngine;
+
+namespace Au
+{
+    public abstract class App : MonoBehaviour
+    {
+        private void Start()
+        {
+            DontDestroyOnLoad(gameObject);
+            OnStart();
+        }
+
+        protected TSApp tsApp;
+
+        protected abstract void OnStart();
+
+        protected object rootComponent;
+
+        public virtual void Bootstrap<T>()
+        {
+            rootComponent = Container.root.Resolve<T>();
+        }
+    }
+}
